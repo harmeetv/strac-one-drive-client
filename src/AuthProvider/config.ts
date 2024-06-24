6868bee4adef477095642d3a1b1f54cd39392146
harmeetv <hrmtsngh18@gmail.com>
2024-06-25 00:42:35 +0530

    Update config.ts

--- src/AuthProvider/config.ts
+++ src/AuthProvider/config.ts
@@ -3,9 +3,9 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
   auth: {
-    clientId: "52b573ba-c4e5-42e5-9855-1ad0536ae3c3", // Replace with your client ID
-    authority: "https://login.microsoftonline.com/Common", // Replace with your tenant ID
-    redirectUri: process.env.REACT_APP_ONEDRIVE_REDIRECT_URI
+    clientId: import.meta.env.REACT_APP_ONEDRIVE_CLIENT_ID, // Replace with your client ID
+    authority: "https://login.microsoftonline.com/Common",
+    redirectUri: import.meta.env.REACT_APP_ONEDRIVE_REDIRECT_URI
   },
 };
 
