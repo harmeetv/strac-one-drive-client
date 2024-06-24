d668c15938c5870a5d02c8e5419c212371fc93be
harmeetv <hrmtsngh18@gmail.com>
2024-06-25 00:21:52 +0530

    Add files via upload

--- src/AuthProvider/config.ts
+++ src/AuthProvider/config.ts
@@ -5,7 +5,7 @@ const msalConfig = {
   auth: {
     clientId: "52b573ba-c4e5-42e5-9855-1ad0536ae3c3", // Replace with your client ID
     authority: "https://login.microsoftonline.com/Common", // Replace with your tenant ID
-    redirectUri: process.env.DOMAIN
+    redirectUri: process.env.REACT_APP_ONEDRIVE_REDIRECT_URI
   },
 };
 
